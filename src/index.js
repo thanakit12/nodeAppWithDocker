909b9f8d7a41d639a0ff2c7e28d9345512fe60ff
Thanakrit Haruehansapong <thanakrit-srs@nuttapocothsMBP.Sirisoft-GW>
2020-07-30 12:06:50 +0700

    add hello function

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const port = 80;
 app.get('/',(req,res) => {
     res.send("Hello")
 })
+
+app.get('/hello',(req,res) => {
+    res.send("Hello World")
+})
 .listen(port,() => {
     console.log(`------Server Starting on Port ${port} -----------`)
 })
